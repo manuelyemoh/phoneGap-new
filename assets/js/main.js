dc6afc4a62d74b3e1bda90d104bbe3f5a855162d
manuelyemoh <SuperMac3@SuperMac3.local>
2013-03-18 15:37:44 +0000

    slide is working?
    slide is working?!!?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,55 @@
- var jQT = new $.jQTouch({
-                icon: 'jqtouch.png',
-                addGlossToIcon: false,
-                startupScreen: 'jqt_startup.png',
-                statusBar: 'black',
-            });
+$.jQTouch({
+	icon: 'jqtouch.png',
+	statusBar: 'black-translucent',
+	
+});
 
-            $(function(){
-                jQT.addAnimation({
-                    name: 'reveal',
-                    selector: '.revealme'
-                });
-            });
+//alert($('.middleContent').height());
+//alert($('#tab0').width());
 
+//var windowWidth = $('.middleContent').width();
+
+//$('#pageCase0').width(windowWidth);
+//$('#pageCase1').width(windowWidth);
+//$('#pageCase2').width(windowWidth);
+//$('#pageCase3').width(windowWidth);
+
+//$('#pageCase1').css(
+//{
+  //'left': windowWidth
+//});
+
+//var myTabs = new Array[];
+
+//$('#tab0').mouseover(function(){
+	
+	//alert("hit");
+	
+//});
+
+
+	
+//for (var i=0; i<4; i++){
+	
+	//var tabs = $('#tab' + i);
+	//var para = $('p');
+	
+	//tabs.style.opacity=0;
+	
+	//$(tabs).mouseover(function() {
+		
+		//$(tabs+para).css('color', 'white');
+		//$('#blogOption').css('background-color', '#ff6600');
+		//alert("hit"+tabs);
+		
+	//});
+	
+	//$(tabs).mouseout(function() {
+	
+		//$('#blogOption p').css('color', '#999999');
+		//$('#blogOption').css('background-color', '#212121');
+		//alert("hit");
+	//});
+//}
+
+	
