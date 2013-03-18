1bf0e0fcc2005a8913483dedbe89396d547173c1
manuelyemoh <SuperMac3@SuperMac3.local>
2013-03-18 16:32:38 +0000

    new test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,89 @@
-$.jQTouch({
-	icon: 'jqtouch.png',
-	statusBar: 'black-translucent',
+$(document).ready(function() {
+
+	$.jQTouch({
+		icon: 'jqtouch.png',
+		statusBar: 'black-translucent',
+		
+	});
 	
+	$('#tab0').mouseover(function() {
+		
+		$('#tab0').css('background-color', '#ff6600');
+		$('#tab0 p').css('color', 'white');
+	
+	});
+	
+	$('#tab0').mouseout(function() {
+		
+		$('#tab0').css('background-color', '#212121');
+		$('#tab0 p').css('color', '#999999');
+	
+	});
+	
+	$('#tab1').mouseover(function() {
+		
+		$('#tab1').css('background-color', '#ff6600');
+		$('#tab1 p').css('color', 'white');
+	
+	});
+	
+	$('#tab1').mouseout(function() {
+		
+		$('#tab1').css('background-color', '#212121');
+		$('#tab1 p').css('color', '#999999');
+	
+	});
+	
+	$('#tab2').mouseover(function() {
+		
+		$('#tab2').css('background-color', '#ff6600');
+		$('#tab2 p').css('color', 'white');
+	
+	});
+	
+	$('#tab2').mouseout(function() {
+		
+		$('#tab2').css('background-color', '#212121');
+		$('#tab2 p').css('color', '#999999');
+	
+	});
+	
+	$('#tab3').mouseover(function() {
+		
+		$('#tab3').css('background-color', '#ff6600');
+		$('#tab3 p').css('color', 'white');
+	
+	});
+	
+	$('#tab3').mouseout(function() {
+		
+		$('#tab3').css('background-color', '#212121');
+		$('#tab3 p').css('color', '#999999');
+	
+	});
+	
+	$("#tab0").click(function() {
+		
+		$('#tab0').css('background-color', '#212121');
+		$('#tab0 p').css('color', '#999999');
+	  
+	});
+	
+	$("#tab1").click(function() {
+	  
+	  
+	});
+	
+	$("#tab2").click(function() {
+	  
+	  
+	});
+	
+	$("#tab3").click(function() {
+	  
+	  
+	});
+
 });
 
 //alert($('.middleContent').height());
