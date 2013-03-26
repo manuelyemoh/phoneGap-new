b6410833533a86766549888da9fb3f94a734af67
manuelyemoh <SuperMac3@SuperMac3.local>
2013-03-26 11:05:56 +0000

    added test blog post

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,6 @@ $(document).ready(function() {
 		
 	});
 	
-	alert("width"+window.innerWidth);
-	alert("height"+window.innerHeight);
-	$("p").text(window.innerHeight+""+window.innerWidth);
 	$(".tabOver").mouseover(function(){
 		
 		var currentId = $(this).attr('id');
