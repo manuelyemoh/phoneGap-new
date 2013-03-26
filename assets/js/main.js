f6e555b1b6c9ce565098b3ce3db187784ddb9289
manuelyemoh <SuperMac3@SuperMac3.local>
2013-03-26 11:58:08 +0000

    scroll fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	});
 	
 	if (!!('ontouchstart' in window)) {
-			$('guideBlog').touchScroll();
+			$('nav').touchScroll();
 	}
 	
 	$(".tabOver").mouseover(function(){
